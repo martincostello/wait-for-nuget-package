1a738565b022af8c51339794afc079790177577f
martincostello <martin@martincostello.com>
2024-05-24 14:12:22 +0100

    Refactor DI
    Refactor DI out of `Waiter` and add unit tests for service registrations.

---
+++ src/WaitForNuGetPackage/ServiceCollectionExtensions.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Net.Http.Headers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NuGet.Protocol.Catalog;
+using Spectre.Console;
+
+namespace MartinCostello.WaitForNuGetPackage;
+
+internal static class ServiceCollectionExtensions
+{
+    private static readonly ProductInfoHeaderValue _userAgent = CreateUserAgent();
+
+    public static IServiceCollection AddServices(
+        this IServiceCollection services,
+        IReadOnlyCollection<string> args,
+        IAnsiConsole console,
+        CancellationTokenSource cancellationTokenSource)
+    {
+        services.AddSingleton<IAnsiConsole>(console);
+        services.AddSingleton(TimeProvider.System);
+        services.AddHttpClient()
+                .ConfigureHttpClientDefaults((builder) =>
+                {
+                    builder.ConfigureHttpClient((client) => client.DefaultRequestHeaders.UserAgent.Add(_userAgent));
+                    builder.AddStandardResilienceHandler();
+                });
+
+        services.AddHttpClient<ISimpleHttpClient, SimpleHttpClient>();
+        services.AddTransient<ICatalogClient, CatalogClient>();
+        services.AddTransient<ICatalogLeafProcessor, CatalogLeafProcessor>();
+        services.AddSingleton<ICursor, InMemoryCursor>();
+
+        services.AddSingleton<CatalogProcessor>();
+        services.AddSingleton<CatalogProcessorSettings>();
+        services.AddSingleton<PackageWaitContext>();
+
+        services.AddSingleton(cancellationTokenSource);
+
+        // TODO Wire this up to DI nicely
+        // TODO Allow the service index to be specified
+        var settings = new CatalogProcessorSettings()
+        {
+            DefaultMinCommitTimestamp = DateTimeOffset.UtcNow.AddMinutes(-10),
+            ExcludeRedundantLeaves = false,
+        };
+
+        services.AddSingleton(settings);
+
+        services.AddLogging((builder) =>
+        {
+            var level =
+                args.Contains("--verbose", StringComparer.OrdinalIgnoreCase) ?
+                LogLevel.Debug :
+                LogLevel.Warning;
+
+            builder.AddConsole()
+                   .AddFilter("NuGet", LogLevel.Warning)
+                   .AddFilter("Polly", LogLevel.Error)
+                   .AddFilter("System.Net.Http.HttpClient", LogLevel.Warning)
+                   .SetMinimumLevel(level);
+        });
+
+        return services;
+    }
+
+    private static ProductInfoHeaderValue CreateUserAgent()
+    {
+        var version = Waiter.Version;
+
+        // Truncate the Git commit SHA to 7 characters, if present
+        int indexOfPlus = version.IndexOf('+', StringComparison.Ordinal);
+
+        if (indexOfPlus > -1 && indexOfPlus < version.Length - 1)
+        {
+            string hash = version[(indexOfPlus + 1)..];
+
+            if (hash.Length > 7)
+            {
+                version = version[..(indexOfPlus + 8)];
+            }
+        }
+
+        return new ProductInfoHeaderValue("WaitForNuGetPackage", version);
+    }
+}
--- src/WaitForNuGetPackage/Waiter.cs
+++ src/WaitForNuGetPackage/Waiter.cs
@@ -1,11 +1,8 @@
 ﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Net.Http.Headers;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using NuGet.Protocol.Catalog;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
@@ -19,8 +16,6 @@ internal static class Waiter
     //// See https://github.com/NuGet/Samples/blob/ec30a2b7c54c2d09e5a476444a2c7a8f2f289d49/CatalogReaderExample
 
     public static readonly string Version = typeof(Waiter).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
-
-    private static readonly ProductInfoHeaderValue _userAgent = CreateUserAgent();
 
     /// <summary>
     /// Waits for a new version of one or more NuGet packages to be published as an asynchronous operation.
@@ -36,54 +31,9 @@ internal static class Waiter
         IReadOnlyCollection<string> args,
         CancellationToken cancellationToken)
     {
-        var services = new ServiceCollection();
-
-        services.AddSingleton<IAnsiConsole>(console);
-        services.AddSingleton(TimeProvider.System);
-
-        services.AddHttpClient()
-                .ConfigureHttpClientDefaults((builder) =>
-                {
-                    builder.ConfigureHttpClient((client) => client.DefaultRequestHeaders.UserAgent.Add(_userAgent));
-                    builder.AddStandardResilienceHandler();
-                });
-
-        services.AddHttpClient<ISimpleHttpClient, SimpleHttpClient>();
-        services.AddTransient<ICatalogClient, CatalogClient>();
-        services.AddTransient<ICatalogLeafProcessor, CatalogLeafProcessor>();
-        services.AddSingleton<ICursor, InMemoryCursor>();
-
-        services.AddSingleton<CatalogProcessor>();
-        services.AddSingleton<CatalogProcessorSettings>();
-        services.AddSingleton<PackageWaitContext>();
-
         using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-        services.AddSingleton(cts);
 
-        // TODO Wire this up to DI nicely
-        // TODO Allow the service index to be specified
-        var settings = new CatalogProcessorSettings()
-        {
-            DefaultMinCommitTimestamp = DateTimeOffset.UtcNow.AddMinutes(-10),
-            ExcludeRedundantLeaves = false,
-        };
-
-        services.AddSingleton(settings);
-
-        services.AddLogging((builder) =>
-        {
-            var level =
-                args.Contains("--verbose", StringComparer.OrdinalIgnoreCase) ?
-                LogLevel.Debug :
-                LogLevel.Warning;
-
-            builder.AddConsole()
-                   .AddFilter("NuGet", LogLevel.Warning)
-                   .AddFilter("Polly", LogLevel.Error)
-                   .AddFilter("System.Net.Http.HttpClient", LogLevel.Warning)
-                   .SetMinimumLevel(level);
-        });
-
+        var services = new ServiceCollection().AddServices(args, console, cts);
         var registrar = new TypeRegistrar(services);
 
         var app = new CommandApp<WaitCommand>(registrar);
@@ -97,25 +47,5 @@ internal static class Waiter
         });
 
         return await app.RunAsync(args);
-    }
-
-    private static ProductInfoHeaderValue CreateUserAgent()
-    {
-        var productVersion = Version;
-
-        // Truncate the Git commit SHA to 7 characters, if present
-        int indexOfPlus = productVersion.IndexOf('+', StringComparison.Ordinal);
-
-        if (indexOfPlus > -1 && indexOfPlus < productVersion.Length - 1)
-        {
-            string hash = productVersion[(indexOfPlus + 1)..];
-
-            if (hash.Length > 7)
-            {
-                productVersion = productVersion[..(indexOfPlus + 8)];
-            }
-        }
-
-        return new ProductInfoHeaderValue("WaitForNuGetPackage", productVersion);
     }
 }
