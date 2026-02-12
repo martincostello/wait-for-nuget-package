0b50ad9cbd715e1feba3c7be8bf75418111dcb2a
Martin Costello <martin@martincostello.com>
2026-02-12 10:04:15 +0000

    Fix link rendering in table (#547)
    Do not render links in table cells if not supported.

--- src/WaitForNuGetPackage/WaitCommand.cs
+++ src/WaitForNuGetPackage/WaitCommand.cs
@@ -39,7 +39,8 @@ internal sealed class WaitCommand(
 
         foreach (var package in packages.DesiredPackages.OrderBy((p) => p.Id, comparer).ThenBy((p) => p.Version, comparer))
         {
-            table.AddRow($"[link=https://www.nuget.org/packages/{package.Id}]{package.Id}[/]", package.Version);
+            string packageId = console.Profile.Capabilities.Links ? $"[link=https://www.nuget.org/packages/{package.Id}]{package.Id}[/]" : package.Id;
+            table.AddRow(packageId, package.Version);
         }
 
         console.Write(table);
